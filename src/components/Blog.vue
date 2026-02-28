2a50d4fe7f933d2d3261f8833d1156a98c1a5125
kona-jk <jannatul.ferdous.kona.jk@gmail.com>
2026-02-28 11:35:50 -0500

    Adjust blog page padding

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -7,8 +7,8 @@ import Contact from '@/components/Contact.vue';
   <section class="stories-section overflow-hidden">
       <div class="container mx-auto text-yellow md:px-12">
          <!--Flex Container-->
-        <div class="flex flex-col items-center justify-between md:flex-row ">
-          <div class="py-6 md:pt-0 ">
+        <div class="flex flex-col pt-30 items-center justify-between md:flex-row ">
+          <div class="py-6 md::pt-0 ">
             <h2 class="text-3xl md:text-6xl">
               "stories ,"
               <br /><span>historias,</span> <br /><span>ictopii,</span> <br /><span>iotopice</span>
@@ -23,7 +23,7 @@ import Contact from '@/components/Contact.vue';
           </div>
         </div>
         <!--grid box-->
-        <div class="grid pt-20 md:pt-40 px-6 md:grid-cols-2 md:px-0">
+        <div class="grid pt-20 md:pt-40 px-6 md:grid-cols-2 md:px-0 md:pb-6">
           <div class="">
             <h2 class=" text-xl md:text-4xl">Let,s hear them.</h2>
           </div>
